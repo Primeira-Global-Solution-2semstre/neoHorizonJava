19779984f41ed5290894219afbac3bc6f5aa178f
BragaSoterasVinicius <viniciussoteras@gmail.com>
2026-06-06 22:34:02 -0300

    fix: findAll predictions

--- src/main/java/br/com/fiap/neohorizon/controller/PredictorController.java
+++ src/main/java/br/com/fiap/neohorizon/controller/PredictorController.java
@@ -4,10 +4,14 @@ import br.com.fiap.neohorizon.assembler.PredictionAssembler;
 import br.com.fiap.neohorizon.dto.PredictionInputDto;
 import br.com.fiap.neohorizon.model.Prediction;
 import br.com.fiap.neohorizon.service.PredictionService;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/predict")
@@ -24,12 +28,21 @@ public class PredictorController {
     @PostMapping()
     public EntityModel<Prediction> generatePrediction(@RequestBody PredictionInputDto predictionInputDTO){
         Prediction prediction = predictionService.generatePrediction(predictionInputDTO.getPeriodo(), predictionInputDTO.getSpaceObjectTag());
-
         return predictionAssembler.toModel(prediction);
     }
 
     @GetMapping()
-    public List<Prediction> findAll(){
-        return predictionService.findAllPredictions();
+    public CollectionModel<EntityModel<Prediction>> findAll(){
+        List<EntityModel<Prediction>> predictions =
+                predictionService.findAllPredictions()
+                        .stream()
+                        .map(predictionAssembler::toModel)
+                        .toList();
+
+        return CollectionModel.of(
+                predictions,
+                linkTo(methodOn(PredictorController.class).findAll())
+                        .withSelfRel()
+        );
     }
 }
